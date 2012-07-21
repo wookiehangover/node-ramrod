59ce2903db5a0f7a6be646aaa5fa4f2bf98bcced
Sam Breed <sam@quickleft.com>
2012-07-21 13:13:00 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+var namedParam    = /:\w+/g;
+var splatParam    = /\*\w+/g;
+var escapeRegExp  = /[-[\]{}()+?.,\\^$|#\s]/g;
+
+function Ramrod( routes ){
+  this.routes = {};
+
+  if( routes ){
+    for(var path in routes ){
+      if( routes.hasOwnProperty( path ) ){
+
+        if( util.isRegExp( routes[path] ) ){
+          this.routes[path] = routes[path];
+        }
+
+        if( typeof routes[path] == "function" ){
+          this.add( path, routes[path] );
+        }
+
+      }
+    }
+  }
+}
+
+util.inherits(Ramrod, EventEmitter);
+
+Ramrod.prototype.add = function( route, name, callback ){
+  if( !callback && typeof name == "function") {
+    callback = name;
+  }
+
+  if( !name || typeof name == "function" ) {
+    name = route;
+  }
+
+  if( !util.isRegExp(route) ){
+    route = this._routeToRegExp( route );
+  }
+
+  if( callback ){
+    this.on(name, callback);
+  }
+
+  this.routes[name] = route;
+};
+
+Ramrod.prototype._routeToRegExp = function(route) {
+  route = route.replace(escapeRegExp, '\\$&')
+               .replace(namedParam, '([^\/]+)')
+               .replace(splatParam, '(.*?)');
+  return new RegExp('^' + route + '$');
+};
+
+Ramrod.prototype.dispatch = function( req, res ){
+  var params;
+
+  for(var path in this.routes){
+    if( (params = this.routes[path].exec( req.url.substr(1) )) ){
+      return this.emit.apply(this, [path, req, res].concat( params.slice(1) ));
+    }
+  }
+
+  this.emit('*', req, res);
+};
+
+module.exports = function( routes ){
+  return new Ramrod( routes );
+};
+
+module.exports.Ramrod = Ramrod;
---
+++ test/index.js
@@ -0,0 +1,121 @@
+/*global describe, it, after, before, beforeEach*/
+var ramrod = require('../index');
+var assert = require('assert');
+
+describe('Router', function(){
+
+  describe('constr', function(){
+    it('should instantiate 2 ways', function(){
+      assert.ok( ramrod() );
+      assert.ok( new ramrod.Ramrod );
+    });
+
+    it('should take a routing table as an argument', function(){
+      var router = ramrod({
+        'foo/:bar': function( req, res ){}
+      });
+
+      assert.ok( router.routes['foo/:bar'] );
+    });
+
+    it('lets you make custom regex routes w/o an fn handler', function( done ){
+      var router = ramrod({
+        'foo': /^foo|bar/
+      });
+
+      assert.ok( router.routes.foo );
+
+      // lets have a little integration test just for shits
+      var count = 0;
+
+      router.on('foo', function( req ){
+        count += 1;
+        if( count === 2 ) done();
+      });
+
+      router.dispatch({ url: '/foo'});
+      router.dispatch({ url: '/bar'});
+    });
+  });
+
+  describe('add', function(){
+    beforeEach(function(){
+      this.router = ramrod();
+    });
+
+    it('should add a route to the routes object', function(){
+      this.router.add('foo/bar');
+      assert.ok(this.router.routes['foo/bar']);
+    });
+
+    it('can add a named route', function(){
+      this.router.add('foo/bar', 'foo');
+      assert.ok(this.router.routes.foo);
+    });
+
+    it('will bind a callback if available', function( done ){
+      this.router.add('foo/bar', 'foo', function(){ done(); });
+      this.router.emit('foo');
+    });
+
+    it('will bind a callback even with an anonymous route', function( done ){
+      this.router.add('foo/bar', function(){ done(); });
+      this.router.emit('foo/bar');
+    });
+
+    it('allows regex routes', function(){
+      this.router.add(/foo/, 'foo');
+      assert.ok(this.router.routes.foo);
+    });
+
+    it('even lets you use anonymouse regex routes (like an idiot)', function(){
+      this.router.add(/foo/);
+      assert.ok(this.router.routes['/foo/']);
+    });
+  });
+
+  describe('dispatch', function(){
+    beforeEach(function(){
+      this.router = ramrod();
+    });
+
+    it('should match on an existing route', function( done ){
+      this.router.add('foo', function( req, res ){
+        done();
+      });
+
+      this.router.dispatch({ url: '/foo' });
+
+    });
+
+    it('should fire * when no route matches', function( done ){
+      this.router.on('*', function(){
+        done();
+      });
+
+      this.router.dispatch({ url: '/foo' });
+    });
+
+    it('should pass req and res to the handler', function( done ){
+      this.router.on('*', function( req, res ){
+        assert.equal( req.url, '/foo' );
+        assert.ok( res );
+        done();
+      });
+
+      this.router.dispatch({ url: '/foo'}, {});
+    });
+
+    it('should pass route params as arguments', function( done ){
+      this.router.add('foo/:bar/:biz', function( req, res, bar, biz ){
+        assert.equal( bar, 'hotdog');
+        assert.equal( biz, 'cats');
+        done();
+      });
+
+      this.router.dispatch({ url: '/foo/hotdog/cats' }, null);
+    });
+  });
+
+});
+
