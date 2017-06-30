29a1a2d9ca3d4be81962eb502067599937ddc532
Sam Breed <samuel.breed@gmail.com>
2017-06-29 21:30:23 -0700

    hey, JavaScript has classes now!

--- index.js
+++ index.js
@@ -1,55 +1,45 @@
-var EventEmitter = require('events').EventEmitter
-var util = require('util')
-var parseUrl = require('url').parse
-var parseQuerystring = require('querystring').parse
+'use strict'
 
-var namedParam = /:\w+/g
-var splatParam = /\*\w+/g
-var escapeRegExp = /[-[\]{}()+?.,\\^$|#\s*]/g
-var namespaced = /^[\w\/:$\-_.+!*(),]+\|(\w+)$/
+const { EventEmitter } = require('events')
+const parseUrl = require('url').parse
+const parseQuerystring = require('querystring').parse
 
-function Ramrod (routes) {
-  this.routes = {}
+const namedParam = /:\w+/g
+const splatParam = /\*\w+/g
+const escapeRegExp = /[-[\]{}()+?.,\\^$|#\s*]/g
+const namespaced = /^[\w/:$\-_.+!*(),]+\|(\w+)$/
 
-  if (routes) {
-    for (let path in routes) {
-      if (routes.hasOwnProperty(path)) {
-        const route = routes[path]
-        if (toString.call(route) === '[object RegExp]') {
-          this.routes[path] = route
-        } else
-        if (toString.call(route) === '[object Function]') {
-          this.add(path, route)
-        } else {
-          this.add(path)
+function routeToRegExp (route) {
+  route = route.replace(escapeRegExp, '\\$&')
+               .replace(namedParam, '([^/]+)')
+               .replace(splatParam, '(.*?)')
+  return new RegExp('^/' + route + '$')
+}
+
+function next () {}
+
+class Router extends EventEmitter {
+  constructor (routes) {
+    super()
+    this.routes = {}
+    if (routes) {
+      for (let path in routes) {
+        if (routes.hasOwnProperty(path)) {
+          const route = routes[path]
+          if (toString.call(route) === '[object RegExp]') {
+            this.routes[path] = route
+          } else
+          if (toString.call(route) === '[object Function]') {
+            this.add(path, route)
+          } else {
+            this.add(path)
+          }
         }
       }
     }
   }
-}
 
-util.inherits(Ramrod, EventEmitter)
-
-Ramrod.prototype.add = function (route, name, callback) {
-  if (!callback && typeof name === 'function') {
-    callback = name
-  }
-  if (!name || typeof name === 'function') {
-    name = route
-  }
-  if (toString.call(route) !== '[object RegExp]') {
-    route = this._routeToRegExp(route)
-  }
-  if (callback) {
-    this.on(name, callback)
-  }
-  this.routes[name] = route
-};
-
-['get', 'post', 'put', 'del', 'options'].forEach(function (method) {
-  var methodName = method === 'del' ? 'delete' : method
-
-  Ramrod.prototype[method] = function (route, name, callback) {
+  add (route, name, callback) {
     if (!callback && typeof name === 'function') {
       callback = name
     }
@@ -57,7 +47,62 @@ Ramrod.prototype.add = function (route, name, callback) {
       name = route
     }
     if (toString.call(route) !== '[object RegExp]') {
-      route = this._routeToRegExp(route)
+      route = routeToRegExp(route)
+    }
+    if (callback) {
+      this.on(name, callback)
+    }
+    this.routes[name] = route
+  }
+
+  dispatch (req, res) {
+    let params
+    let routeMethod
+    const url = parseUrl(req.url)
+    const method = req.method && req.method.toLowerCase()
+
+    this.emit('before', req, res, next)
+
+    for (const path in this.routes) {
+      if ((params = this.routes[path].exec(url.pathname))) {
+        let args = [ path, req, res ]
+
+        if (params.length >= 1) {
+          args = args.concat(params.slice(1))
+        }
+
+        if (url.query) {
+          args.push(parseQuerystring(url.query))
+        }
+
+        routeMethod = namespaced.exec(path)
+
+        if (routeMethod && routeMethod[1]) {
+          if (routeMethod[1] === method) {
+            return this.emit.apply(this, args)
+          }
+        } else {
+          return this.emit.apply(this, args)
+        }
+      }
+    }
+
+    this.emit('*', req, res, next)
+  }
+}
+
+['get', 'post', 'put', 'del', 'options'].forEach(function (method) {
+  const methodName = method === 'del' ? 'delete' : method
+
+  Router.prototype[method] = function (route, name, callback) {
+    if (!callback && typeof name === 'function') {
+      callback = name
+    }
+    if (!name || typeof name === 'function') {
+      name = route
+    }
+    if (toString.call(route) !== '[object RegExp]') {
+      route = routeToRegExp(route)
     }
     if (callback) {
       this.on(name + '|' + methodName, callback)
@@ -66,51 +111,8 @@ Ramrod.prototype.add = function (route, name, callback) {
   }
 })
 
-Ramrod.prototype._routeToRegExp = function (route) {
-  route = route.replace(escapeRegExp, '\\$&')
-               .replace(namedParam, '([^\/]+)')
-               .replace(splatParam, '(.*?)')
-  return new RegExp('^\/' + route + '$')
-}
-
-function next () {}
-
-Ramrod.prototype.dispatch = function (req, res) {
-  var params, routeMethod
-  var url = parseUrl(req.url)
-  var method = req.method && req.method.toLowerCase()
-
-  this.emit('before', req, res, next)
-
-  for (var path in this.routes) {
-    if ((params = this.routes[path].exec(url.pathname))) {
-      var args = [ path, req, res ]
-
-      if (params.length >= 1) {
-        args = args.concat(params.slice(1))
-      }
-
-      if (url.query) {
-        args.push(parseQuerystring(url.query))
-      }
-
-      routeMethod = namespaced.exec(path)
-
-      if (routeMethod && routeMethod[1]) {
-        if (routeMethod[1] === method) {
-          return this.emit.apply(this, args)
-        }
-      } else {
-        return this.emit.apply(this, args)
-      }
-    }
-  }
-
-  this.emit('*', req, res, next)
-}
-
 module.exports = function (routes) {
-  return new Ramrod(routes)
+  return new Router(routes)
 }
 
-module.exports.Ramrod = Ramrod
+module.exports.Ramrod = Router
